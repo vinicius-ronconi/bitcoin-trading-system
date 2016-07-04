fa89735b09dac8c7a1a3affa6e6c15ee46bd569c
Vinicius Ronconi <vinicius.ronconi@gmail.com>
2016-07-04 17:15:32 -0300

    + Add an example of how to get balance from Foxbit

---
+++ trading_system/trading_systems/manual/accounts.py
@@ -0,0 +1,20 @@
+from trading_system.api.clients import BlinkTradeClient
+from trading_system.api import consts
+from trading_system.trading_systems.settings import *
+
+
+class ManualTradingSystem(object):
+    def __init__(self):
+        self.client = BlinkTradeClient(
+            consts.Environment.PRODUCTION,
+            consts.Currency.BRAZILIAN_REAIS,
+            consts.Broker.FOXBIT,
+            BLINKTRADE_KEY, BLINKTRADE_SECRET,
+        )
+
+    def get_balance(self):
+        return self.client.account.get_balance()
+
+
+if __name__ == '__main__':
+    print ManualTradingSystem().get_balance()
