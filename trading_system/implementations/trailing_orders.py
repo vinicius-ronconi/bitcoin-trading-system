dd7c4c693704f6d14c078d7e38b7620bc72abf3f
Vinicius Ronconi <vinicius.ronconi@gmail.com>
2016-08-27 11:06:40 -0300

    r Rename TrailingOrderImpl class

--- trading_system/implementations/trailing_orders.py
+++ trading_system/implementations/trailing_orders.py
@@ -3,7 +3,7 @@ from trading_system.systems.executor import SystemExecutor
 from trading_system.systems.trailing_orders import TrailingOrders
 
 
-class BitfinexTrailingOrder(object):
+class TrailingOrderImpl(object):
     def __init__(self, client_type):
         client = ApiClientFactory().make_client(client_type)
         self.system = TrailingOrders(client)
@@ -13,4 +13,4 @@ class BitfinexTrailingOrder(object):
         executor.execute()
 
 if __name__ == '__main__':
-    BitfinexTrailingOrder(ApiClientFactory.BITFINEX).run()
+    TrailingOrderImpl(ApiClientFactory.BITFINEX).run()
